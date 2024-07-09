c2a89b6306f2d1b6876a91f6b8473f2821a9e2f2
tlarseth <vegeta.gg42@gmail.com>
2024-07-08 21:07:19 -0500

    cleaning up code

--- hello3.py
+++ hello3.py
@@ -1,13 +1,12 @@
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            x = float(input("What's x? "))
+            return float(input(prompt))
         except ValueError:
             print("That is not a number, please only enter numbers.")
-        else:
-            return x
+        pass
 main()
