82003fc5c95aca0b52a322cd525905256e9f2e37
tlarseth <vegeta.gg42@gmail.com>
2024-09-14 15:28:51 -0500

    Adding new files Adding new files

--- IT-140 Project.py
+++ IT-140 Project.py
@@ -6,13 +6,28 @@
 #Create Win/Lose parameters
 
 # Move between rooms with error handling
+
+class Items:
+    def __init__(self, name, disciption):
+        self.name = name
+        self.position = disciption
+
+
+sword = Weapon("Master Sword", "It's dangerous to go alone, take this.")
+grenade = Weapon("Holy Hand Grenade of Antioch", "Three shall be the number thou shalt count, and the number of the counting shall be three. Four shalt thou not count, neither count thou two, excepting that thou then proceed to three. Five is right out. Once the number three, being the third number, be reached, then lobbest thou thy Holy Hand Grenade of Antioch towards thy foe, who, being naughty in my sight, shall snuff it")
+mushroom = Items("Magic Mushroom", "Why are my pants now shorts?")
+game = Items("Hatsune Miku Colorful Stage", "Blame my 15 year old daughter for this one")
+doll = Items("Freddy Fazbear Plushy", "Can you survive five nights?")
+paimon = Items("Paimon", "Emergency Rations:  Just in case you get hungry")
+
+
 rooms = {
     'Great Hall': {'South': 'Bedroom'},
     'Bedroom': {'North': 'Great Hall', 'West': 'Cellar'},
     'Cellar': {'West': 'Exit', 'East': 'Bedroom'},
     'Exit': {'East': 'Cellar'}
 }
-
+items = {'Great Hall': 'Sword'}
 print ('To navigate type the cardinal direction you want to move in "North, South, East, and West"  Type "Exit" to leave the game')
 
 
@@ -22,13 +37,18 @@ while current_room != "Exit":
     print("You are in the", current_room)
     print("Which direction will you go?")
     print("The possible directions are:", rooms[current_room])
+    print("There is a {} in this room".format(items[current_room]))
 
-    direction = input(">>> ").title()
+    get = input(">>> ").title()
+    if get in items[current_room]:
+        items.pop("get", None)
+    print("There is a {} in this room".format(items[current_room]))
+    #direction = input(">>> ").title()
 
-    if direction in rooms[current_room]:
-        current_room = rooms[current_room][direction]
-    else:
-        print("Can not do that.\nTry another direction.")
+    #if direction in rooms[current_room]:
+       # current_room = rooms[current_room][direction]
+    #else:
+        #print("Can not do that.\nTry another direction.")
 
 
-print("Congratulations you made it safely.")
+#print("Congratulations you made it safely.")
---
+++ Paycheck Calculator.py
@@ -0,0 +1,23 @@
+# ask for input for employee payrate and total hours worked
+emp_payrate = float(input("What is the employee's payrate?"))
+emp_total_hours = float(input("What is the employee's total hours for the week?"))
+
+# check if total hours are over 40 hours
+# If they are create new variable for OT hours and reg hours
+
+if emp_total_hours > 40:
+    reg_hours = 40
+    ot_hours = emp_total_hours - 40
+else:
+    reg_hours = emp_total_hours
+    ot_hours = 0
+
+# calculate pay for each variable
+
+reg_paycheck = float(reg_hours * emp_payrate)
+ot_payrate = emp_payrate * 1.5
+ot_paycheck = ot_hours * ot_payrate
+total_paycheck = (reg_paycheck + ot_paycheck)
+# Output total paycheck
+
+print ("Your paycheck will be ${} based on {} regular hours and {} overtime hours worked this week.".format(total_paycheck, reg_hours, ot_hours))
