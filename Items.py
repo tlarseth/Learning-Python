958284826b2acb176e261cd977c9cf876c1b40e0
T-Lar Seth (Dae'Loki) <VEGETA.GG42@GMAIL.COM>
2024-09-19 23:13:51 -0500

    Added and updated files

--- Items.py
+++ Items.py
@@ -1,9 +1,3 @@
-# Fantasy Text Game
-# Created by Kyle "Dae'Loki" Mann
-# Created for SNHU IT-140
-
-
-
 rooms = {'Training Camp': {'name': 'the Training Camp', 'Go North': 'Clearing', 'item': 'None'},
          'Clearing': {'name': 'Clearing', 'Go South': 'Training Camp', 'Go North': 'Forest Maze', 'Go East': 'Cave', 'item': 'Master Sword'},
          'Cave': {'name': 'Cave', 'Go West': 'Clearing', 'item': 'Holy Hand Grenade of Antioch'},
@@ -15,7 +9,7 @@ rooms = {'Training Camp': {'name': 'the Training Camp', 'Go North': 'Clearing', 
 }
 
 
-print('Magical Text Adventure Game\n')
+print('Wizard Text Adventure Game\n')
 print('Collect 6 items to win the game, or be defeated by the Darkness.')
 print('Move commands: Go South, Go North, Go East, Go West')
 print('Add to Inventory: Get Item')
@@ -24,11 +18,11 @@ current_room = rooms['Training Camp']
 directions = ['Go North', 'Go South', 'Go East', 'Go West']
 Item = ['Master Sword', 'Holy Hand Grenade of Antioch', 'Needler', 'Project Sekai Game', 'Magic Mushroom', 'Dragon Balls']
 Inventory = {}
-user_inv = list(Inventory.values())
+
 while current_room != 'Exit':
     print('-' * 42)
     print('You are in the {}'.format(current_room['name']))
-    print('Your current inventory: {}'.format (list(Inventory.values())))
+    print('Your current inventory: {}'.format (Inventory))
     if current_room['item']:
         print('Item in room: {}'.format(''.join(current_room['item'])))
         print('')
@@ -38,25 +32,18 @@ while current_room != 'Exit':
     if user_command in directions:
         if user_command in current_room:
             current_room = rooms[current_room[user_command]]
-            if (current_room['name'] == 'Mysterious Room') and (len(Inventory.keys()) != 5):
-                print('You lose, but unfortunately the Darkness took the cake with it.')
+            if current_room['name'] == 'Exit':
+                print('You win, but unfortunately the Darkness took the cake with it.')
                 break
-            else:
-                print('You see an item that is up for grabs...hopefully it is not a mimic...')
         else:
-            print('\nA mysterious force prevents you from going that way.')
-        if current_room['name'] == 'Exit':
-            print ("Congradulations on defeating the darkness, you didn't even have to cast magic missile to do it")
-            break
+            print('\nChoose another path.')
     elif user_command == 'Get Item':
         if current_room['item'] != 'none':
             Inventory['Item' + str(len(Inventory.keys()) + 1)] = current_room['item']
             # Inventory.append(current_room['item'])
 
             print("You acquired : ", current_room['item'])
-            print ()
-            print ('Current Inventory:')
-            print(user_inv)
+            print(Inventory)
             current_room['item'] = 'none'
         else:
             print("No items to collect in this room")
@@ -67,4 +54,4 @@ while current_room != 'Exit':
         break
 
     else:
-        print('Invalid input')
+       print('Invalid input')
