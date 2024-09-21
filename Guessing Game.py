24140eab0c649e07b499b74d5f1093ed615a3e83
T-Lar Seth (Dae'Loki) <VEGETA.GG42@GMAIL.COM>
2024-09-20 19:34:11 -0500

    Add files via upload

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,8 +1,5 @@
 import random
 
-seedVal = int(input("What seed should be used? "))
-random.seed(seedVal)
-
 lower = int(input("What is the lower bound?"))
 upper = int(input("What is the upper bound?"))
 
@@ -12,17 +9,20 @@ while upper <= lower:
     upper = int(input("What is the upper bound?"))
 
 rand_num = random.randint(lower, upper)
-
+counter = 0
 while True:
 
     guess = int(input("What is your guess?"))
-
+    counter += 1
     while guess < lower or guess > upper:
         print("What is your guess?")
         guess = int(input("What is your guess?"))
 
+
+
     if guess == rand_num:
         print("You got it!")
+        print ("It took you {} tries to get it".format(counter))
         break
 
     if guess < rand_num:
