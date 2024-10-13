a9db751f4a51b645eb3db1553c5e3279ff350e2e
daeloki <vegeta.gg42@gmail.com>
2024-10-12 21:59:36 -0500

    Code Fixes

---
+++ Movement.py
@@ -0,0 +1,10 @@
+# Dictionary of rooms, items, and directions
+rooms = {'Training Camp': {'name': 'the Training Camp', 'North': 'Clearing', 'item': 'None', 'description': 'Null'},
+         'Clearing': {'name': 'Clearing', 'South': 'Training Camp', 'North': 'Forest Maze', 'East': 'Cave', 'item': 'Master Sword', 'description': 'A mysterious sword that somehow shoots laser beams'},
+         'Cave': {'name': 'Cave', 'West': 'Clearing', 'item': 'Holy Hand Grenade of Antioch', 'description': 'And the number you count to is three, no more no less'},
+         'Forest Maze': {'name': 'Forest Maze', 'South': 'Clearing', 'East': 'Camp Site', 'item': 'BFG', 'description': 'Clears a room of enemies in a huge energy flash'},
+         'Camp Site': {'name': 'Camp Site', 'West': 'Forest Maze', 'East': 'Graveyard', 'item': 'Project Sekai Game', 'description': 'Why do I all of a sudden want to tap the screen rapidly in beat'},
+         'Graveyard': {'name': 'Graveyard', 'South': 'Mysterious Room', 'West': 'Camp Site', 'item': 'Magic Mushroom', 'description': 'Hopefully this is not deadly'},
+         'Mysterious Room': {'name': 'Mysterious Room', 'South': 'Exit', 'North': 'Graveyard', 'item': 'Dragon Balls', 'description': 'What is your wish, I do not have all day'},
+         'Exit': {'name': 'Exit', 'North': 'Mysterious Cave', 'item': 'None', 'description': 'Null'}
+}
